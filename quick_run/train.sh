1de1e582cd1f86db89cda5849b1ca716c36fca6a
Shuhao Lai <shuhaolai18@gmail.com>
2021-12-04 17:41:07 -0500

    Updated scripts

--- quick_run/train.sh
+++
@@ -1,4 +0,0 @@
-python main.py \
-  --mode train \
-  --data_dir  /home/shuhao/Downloads/dataset \
-  --save_dir /home/shuhao/Downloads/
---
+++ scripts_shuhao/train_lr_search.sh
@@ -0,0 +1,17 @@
+LR=$1
+
+DATA=/raid/home/slai16/ObjectDetectionRL/dataset
+ROOT=/raid/home/slai16/ObjectDetectionRL/aeroplane_lr_${LR}
+mkdir ${ROOT}
+mkdir ${ROOT}/checkpoints
+mkdir ${ROOT}/stats
+
+python main.py \
+  --mode train \
+  --batch_size 100 \
+  --data_dir ${DATA}\
+  --save_dir ${ROOT}/checkpoints/ \
+  --stats_dir ${ROOT}/stats/aeroplane.json \
+  --learning_rate ${LR} \
+  --rl_algo DQN \
+  --cls aeroplane
