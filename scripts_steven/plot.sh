11252ef475a74d2e65643f528da8ad3ceb5ffa21
Steven Tan <wtan12@jhu.edu>
2021-12-05 22:37:37 -0500

    update code for plotting

--- scripts_steven/plot.sh
+++ scripts_steven/plot.sh
@@ -1,6 +1,9 @@
 ROOT=/home/steven/Code/GITHUB/ObjectDetectionRL
 
-python ${ROOT}/util/result_analyze.py \
-  --mode lr \
-  --stats_dir ${ROOT}/stats/ \
-  --save_dir ${ROOT}/stats/
+for class in aeroplane bicycle bird car; do
+  python ${ROOT}/util/result_analyze.py \
+    --mode model \
+    --cls $class \
+    --stats_dir ${ROOT}/stats/ \
+    --save_dir ${ROOT}/stats/
+done
